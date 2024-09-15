2214b4d9ca2a512a8f1517152d5d7c132b67cbd9
luca <wjddhks1073@hufs.ac.kr>
2024-09-15 22:03:53 +0900

    ObjectSort.py

--- ObjectSort.py
+++ ObjectSort.py
@@ -1,23 +1,26 @@
-#class를 이용한 객체 정렬
-#cmd + d -> 같은 단어 하나씩 차례로 수정하기
-#cmd + shift + l -> 같은 단어를 동시에 모두 수정하기
+#객체 정렬(익명함수 lambda를 사용하여)
+#cmd + d -> 같은 단어 하나씩 차례로 선택하여 수정
+#cmd + shift + l -> 같은 단어 동시에 모두 선택하여 수정 
 
-class Candidate:
+class Participant:
     def __init__(self, test1, test2, test3):
-        self.test1, self.test2, self.test3 = test1, test2, test3
-    
+        self.test1 = test1
+        self.test2 = test2
+        self.test3 = test3
+
 #객체 리스트 생성 
-candidates = [
-    Candidate(100, 85, 90),
-    Candidate(90, 85, 80),
-    Candidate(90, 85, 60),
-    Candidate(60, 10, 50),
-    Candidate(70, 20, 10)
+participants = [
+    Participant(90, 85, 90), 
+    Participant(90, 85, 80), 
+    Participant(80, 30, 60), 
+    Participant(80, 10, 50), 
+    Participant(70, 20, 10), 
 ]
 
-#익명 함수 lambda 이용하기(간단한 정렬 기준인 경우, 사용 가능)
-#key = lambda x: x 에서 x인자에 들어오는 값은 "하나의 객체"이다. 
-candidates.sort(key=lambda x: (x.test1, x.test2, x.test3), reverse=True)
+#sort 함수에 key 인자로 lambda 함수를 작성하였을 때, x인자에 들어오는 값이 하나의 객체다.
+#reverse 함수 대신에 x 객체에 - 붙이면 내림차순 정렬이 가능하다.
+participants.sort(key=lambda x: (x.test1, x.test2, x.test3), reverse = True) 
 
-for i, candidate in enumerate (candidates):
-    print(f'{i + 1}: {candidate.test1}, {candidate.test2}, {candidate.test3}')
+for (i, participant) in enumerate (participants):
+    print(f'{i + 1}: {participant.test1}, {participant.test2}, {participant.test3}')
+
